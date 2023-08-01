974bc40ce65415cc661084fd1d8eb308424a272f
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-01 12:25:57 +0000

    Task 8 - solution

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  const match = [];
+
+  if (startString.length && (typeof startString === 'string')) {
+    set.forEach((string) => {
+      if (string.startsWith(startString)) {
+        match.push(string.slice(startString.length));
+      }
+    });
+  }
+
+  return match.join('-');
+}
