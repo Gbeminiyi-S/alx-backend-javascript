22b3562a012bd6b6deb53ba4217446ce6345d3d6
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-02 06:08:07 +0000

    check for endpoint load >= 5

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,13 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const endpointCount = weakMap.get(endpoint) || 0;
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
 
-  weakMap.set(endpoint, endpointCount + 1);
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 
-  if (endpointCount >= 5) {
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
